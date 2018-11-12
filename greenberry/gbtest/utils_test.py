db79b0fe43e4eb77f160cafbec41fa98190dca92
jlim2 <jlim2@macalester.edu>
2018-11-12 07:33:50 -0600

    Fixed maths_eval (now named infix_eval) to take care of brackets and  made more utils test

--- greenberry/gbtest/utils_test.py
+++ greenberry/gbtest/utils_test.py
@@ -6,14 +6,21 @@ import sys
 sys.path.append("..")
 
 import unittest
-from utils import capture_maths_eval_print
+from utils import capture_infix_eval_print
 
 class GBUtilsTests(unittest.TestCase):
-
-    def test_maths_eval_add(self):
+    def test_infix_eval_plus(self):
+        x = '3+2+1'
+        output = capture_infix_eval_print(x)
+        self.assertEqual(output, '6')
+    def test_infix_eval_minus(self):
         x = '3+2-1'
-        output = capture_maths_eval_print(x)
+        output = capture_infix_eval_print(x)
         self.assertEqual(output, '4')
+    def test_infix_eval_brackets(self):
+        x = '(3 + 2) + 1'
+        output = capture_infix_eval_print(x)
+        self.assertEqual(output, '6')
 
 if __name__ == '__main__':
     unittest.main(exit=False)
--- greenberry/utils.py
+++ greenberry/utils.py
@@ -29,56 +29,46 @@ def eval(str):         #Function to evaluate postfix expression
         i=i+1
     print(S[0])
 
-def maths_eval(string): # previous InfixCon
-    res=''                                                              #Converting Infix expression to postfix
-    exp=string                                                          #Enter the expression to evaluate but mind the brackets in case
-    exp=list(exp)                                                       #Multiply and divide
-    S=[]
-    L=[]
-    i=0
-    while(i<len(exp)):
-        if(exp[i]>='0' and exp[i]<='9'):
-            res=res+exp[i]
+def infix_eval(infix_expr): # previous InfixCon
+    """
+    Convert Infix expression to Postfix and use eval(str) function to evaluate
+    the mathematical expression.
+    input:
+        infix_expr: string containing infix mathematical expression
+    referred:
+        http://interactivepython.org/runestone/static/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html#fig-moveright
+    """
+    OP_PRIORITY = {
+        "*": 0,
+        "/": 0,
+        "+": 1,
+        "-": 1,
+        "(": 2
+    }
+    postfix_expr=''
+    infix_list=list(infix_expr.replace(' ', '')) # clean empty space and convert to list
+    op_stack=[]
 
+    for infix_val in infix_list:
+        if infix_val.isalnum():
+            postfix_expr += infix_val
+        elif infix_val == '(':
+            op_stack.append(infix_val)
+        elif infix_val == ')':
+            top_val = op_stack.pop()
+            while top_val != '(':
+                postfix_expr += top_val
+                top_val = op_stack.pop()
         else:
-            if(len(S)==0 or exp[i]=='(' or exp[i]=='^'):
-                S.append(exp[i])
-            elif(exp[i]==')'):
-                while(S[-1]!='('):
-                    res=res+S[-1]
-                    S.pop()
-                S.pop()
-            elif(exp[i]=='*' or exp[i]=='/'):
-                if(S[-1]=='^'):
-                    while(len(S)>0 and S[-1]!='('):
-                        res=res+S[-1]
-                        S.pop()
-                    S.append(exp[i])
-                elif(S[-1]=='*' or S[-1]=='/'):
-                    while(len(S)>0 and S[-1]!='('  and S[-1]!='^'):
-                        res=res+S[-1]
-                        S.pop()
-                    S.append(exp[i])
-                else:
-                    S.append(exp[i])
-            elif(exp[i]=='+' or exp[i]=='-'):
-                if(S[-1]=='^' or S[-1]=='*' or S[-1]=='/'):
-                    while(len(S)>0 and S[-1]!='('):
-                        res=res+S[-1]
-                        S.pop()
-                    S.append(exp[i])
-                elif(S[-1]=='+' or S[-1]=='-'):
-                    while(len(S)>0 and S[-1]!='('  and S[-1]!='^' and S[-1]!='*' and S[-1]!='/'):
-                        res=res+S[-1]
-                        S.pop()
-                    S.append(exp[i])
-                else:
-                    S.append(exp[i])
-        i=i+1
-    while(len(S)>0):
-        res=res+S[-1]
-        S.pop()
-    eval(res)
+            while (op_stack) and \
+                  (OP_PRIORITY[op_stack[-1]] <= OP_PRIORITY[infix_val]):
+                postfix_expr += op_stack.pop()
+            op_stack.append(infix_val)
+
+    while op_stack:
+        postfix_expr += op_stack.pop()
+
+    eval(postfix_expr)
 
 
 def capture_gb_eval_print(code):
@@ -89,10 +79,10 @@ def capture_gb_eval_print(code):
     output = temp_stdout.getvalue().strip()
     return output
 
-def capture_maths_eval_print(code):
+def capture_infix_eval_print(code):
     temp_stdout = StringIO()
     # redirect stdout to catch print statement from eval function
     with contextlib.redirect_stdout(temp_stdout):
-        maths_eval(code)
+        infix_eval(code)
     output = temp_stdout.getvalue().strip()
     return output
